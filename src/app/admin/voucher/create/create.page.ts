40f717212d48552563e6124d36405cfb6539a8fc
Rizki%20DS <dsrizki0@gmail.com>
2019-12-20 03:42:12 +0700

    create reward admin

--- src/app/admin/voucher/create/create.page.ts
+++ src/app/admin/voucher/create/create.page.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { LoadingService } from 'src/app/shared/service/loading.service';
 import { ApiService } from 'src/app/shared/service/api.service';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
+import { tap, finalize } from 'rxjs/operators';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-create',
@@ -11,13 +15,95 @@ import { ApiService } from 'src/app/shared/service/api.service';
 export class CreatePage implements OnInit {
   fgCreateReward: FormGroup;
 
+  curUser;
+  // Uploaded File URL
+  UploadedFileURL: Observable<string>;
+  // Snapshot of uploading file
+  snapshot: Observable<any>;
+  // Progress in percentage
+  percentage: Observable<number>;
+  // Upload Task 
+  task: AngularFireUploadTask;
+  //File details  
+  fileName:string;
+  fileSize:number;
+  //Status check 
+  isUploading:boolean;
+  isUploaded:boolean;
+
   constructor(
+    private storage: AngularFireStorage,
+    private database: AngularFireDatabase,
     private api: ApiService,
     private loading: LoadingService,
   ) { }
 
   ngOnInit() {
     this.initFormCreateReward();
+  }
+
+  uploadFile(event: FileList){
+
+    this.loading.present()
+    // The File object
+    const file = event.item(0);
+
+    // Validation for Images Only
+    if (file.type.split('/')[0] !== 'image') { 
+      console.error('unsupported file type :( ')
+      return;
+    }
+
+    this.isUploading = true;
+    this.isUploaded = false;
+
+    this.fileName = file.name;
+
+    // The storage path
+    const path = `daurStorage/${new Date().getTime()}_${file.name}`;
+
+    // Totally optional metadata
+    const customMetadata = { app: 'Daur App image' };
+
+    //File reference
+    const fileRef = this.storage.ref(path);
+
+    // The main task
+    this.task = this.storage.upload(path, file, { customMetadata });
+
+    console.log("upload image")
+    // Get file progress percentage
+    this.percentage = this.task.percentageChanges();
+    this.snapshot = this.task.snapshotChanges().pipe(
+      
+      finalize(() => {
+        // Get uploaded file storage path
+        console.log("asdsda")
+        this.UploadedFileURL = fileRef.getDownloadURL();
+        console.log("asdsda",this.UploadedFileURL)
+        this.UploadedFileURL.subscribe(resp=>{
+         /*  this.addImagetoDB({
+            name: file.name,
+            filepath: resp,
+            size: this.fileSize
+          }); */
+
+          let body = {
+            image: resp
+          }
+
+          this.fgCreateReward.controls.image.patchValue(resp);
+          
+          // this.api.createReward(body);
+        
+          this.loading.dismiss()
+
+        },error=>{
+          console.error(error);
+        this.loading.dismiss()
+        })
+      })
+    )
   }
 
   initFormCreateReward(){
@@ -36,7 +122,8 @@ export class CreatePage implements OnInit {
 
     this.loading.present();
     this.loading.dismiss();
-    this.fgCreateReward.addControl('reward_id',new FormControl("V01",Validators.required));
+    let reward = this.database.createPushId()
+    this.fgCreateReward.addControl('reward_id',new FormControl(reward,Validators.required));
     let body = this.fgCreateReward.value;
     this.api.createReward(body);
   }
